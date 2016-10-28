15667935e988978bd49c5750ded8cc804a064a95
RishiD <rishid@gmail.com>
2016-10-27 21:49:19 -0400

    Remove dead code from test

--- tests/turtle_test.c
+++ tests/turtle_test.c
@@ -23,20 +23,17 @@
 static union arg_value _values[TURTLE_MAX_CMD_PARAMS];
 static bool _flag;
 
-static long int p3_call = 0;
-
 static void _no_params(void)
 {
-  _flag = true;
+    _flag = true;
 }
-
 
 static void print5(int a0, int a1, int a2,
                    int a3,int a4,int a5,int a6,int a7,int a8,int a9,int a10,
                    int a11,int a12,int a13,int a14,int a15,int a16,int a17,
                    int a18,int a19)
 {
-    p3_call++;
+    asm("nop");
 }
 
 static void _bool1(bool b)
@@ -58,15 +55,6 @@ static turtle_cmd_list_t _cmds = {
     TURTLE_CMD_NP("_no_params", NULL, _no_params),
     TURTLE_CMD("bool1", "%b", NULL, _bool1),
     TURTLE_CMD("bcdfIs", "%b %c %d %f %I %s", NULL, _bcdfIs),
-#if 0
-    { "print1", "%d", "help1", print1 },
-    { "print2", "%d %d", "help2", print2 },
-    { "print1", "%d", "help1", print1 },
-    { "print3", "%s %d %b", "help3", print3 },
-    { "print4", "", "help4", print0 },
-    { "print5", "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", "help5", print5 },
-#endif
-    //{ "print0", NULL, "help0", 0, false, print0 },
     { NULL, },
 };
 
@@ -82,11 +70,6 @@ CTEST_TEARDOWN(suite) {
 }
 
 CTEST2(suite, no_params) {
-    //turtle_cmd_list_t cmds = {
-    //  TURTLE_CMD_NP("_no_params", NULL, _no_params),
-    //  { NULL, },
-    //};
-    //ASSERT_TRUE(turtle_init(cmds));
     _flag = false;
     ASSERT_TRUE(turtle_execute("_no_params"));
     ASSERT_TRUE(_flag);
@@ -134,46 +117,3 @@ int main(int argc, const char *argv[])
 {
     return ctest_main(argc, argv);
 }
-
-#if 0
-int main()
-{
-    turtle_init(cmds);
-
-    printf("\n\n");
-    printf("-- it's turtle time!\n");
-
-    turtle_execute("print0");
-
-    turtle_execute("try_bool t");
-    turtle_execute("try_bool f");
-    turtle_execute("try_bool 1");
-    turtle_execute("try_bool w");
-
-    turtle_execute("help");
-
-#if 0
-    turtle_execute("print1 4");
-
-    turtle_execute("print2 7 8");
-
-    struct timespec start, end;
-#define NUM_ITERATIONS (20000000)
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    for (int i = 0; i < NUM_ITERATIONS; i++)
-    {
-        //turtle_execute("print4");
-        turtle_execute("print5 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20");
-    }
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-
-    uint64_t duration = (1000000000UL * (end.tv_sec - start.tv_sec)) + (end.tv_nsec - start.tv_nsec);
-
-    printf("%lu p3_call\n", p3_call);
-    printf("%lu iterations took %lu nanoseconds\n", NUM_ITERATIONS, duration);
-    printf("%.03f million items/sec\n", (double)NUM_ITERATIONS / duration * 1000L);
-#endif
-    return 0;
-}
-#endif
